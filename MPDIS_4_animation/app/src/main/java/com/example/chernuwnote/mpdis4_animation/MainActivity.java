5d9eddd7859a6197627681b160ac87e0a2e7c1e4
chernuw <gotovkoromanhzby@gmail.com>
2017-03-02 20:30:12 +0300

    something new

--- MPDIS_4_animation/app/src/main/java/com/example/chernuwnote/mpdis4_animation/MainActivity.java
+++ MPDIS_4_animation/app/src/main/java/com/example/chernuwnote/mpdis4_animation/MainActivity.java
@@ -1,13 +1,119 @@
 package com.example.chernuwnote.mpdis4_animation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @SuppressLint("NewApi")
+    public void startAnimation(View view) {
+        float dest = 0;
+        ImageView aniView = (ImageView) findViewById(R.id.imageView1);
+        switch (view.getId()) {
+
+            case R.id.Button01:
+                dest = 360;
+                if (aniView.getRotation() == 360) {
+                    System.out.println(aniView.getAlpha());
+                    dest = 0;
+                }
+                ObjectAnimator animation1 = ObjectAnimator.ofFloat(aniView, "rotation", dest);
+                animation1.setDuration(2000);
+                animation1.start();
+                // Show how to load an animation from XML
+                // Animation animation1 = AnimationUtils.loadAnimation(this,
+                // R.anim.myanimation);
+                // animation1.setAnimationListener(this);
+                // animatedView1.startAnimation(animation1);
+                break;
+
+            case R.id.Button02:
+                // Shows how to define a animation via code
+                // Also use an Interpolator (BounceInterpolator)
+                Paint paint = new Paint();
+                TextView aniTextView = (TextView) findViewById(R.id.textView1);
+                float measureTextCenter = paint.measureText(aniTextView.getText().toString());
+                dest = 0 - measureTextCenter;
+                if (aniTextView.getX() < 0) {
+                    dest = 0;
+                }
+                ObjectAnimator animation2 = ObjectAnimator.ofFloat(aniTextView,"x", dest);
+                animation2.setDuration(2000);
+                animation2.start();
+                break;
+
+           /* case R.id.Button03:
+                // Demonstrate fading and adding an AnimationListener
+                RelativeLayout mainContainer = (RelativeLayout) findViewById(R.id.layout);
+                LayoutAnimationController controller = AnimationUtils.loadLayoutAnimation(this, R.anim.main_layout_animation);
+                mainContainer.setLayoutAnimation(controller);
+
+                dest = 1;
+                Button button3=(Button)findViewById(R.id.Button03);
+                button3.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.hyperspace_jump));
+                if (aniView.getAlpha() > 0) {
+                    dest = 0;
+                }
+                ObjectAnimator animation3 = ObjectAnimator.ofFloat(aniView,
+                        "alpha", dest);
+                animation3.setDuration(2000);
+                animation3.start();
+                break;*/
+
+            case R.id.Button04:
+
+                ObjectAnimator fadeOut = ObjectAnimator.ofFloat(aniView, "alpha",0f);
+                fadeOut.setDuration(2000);
+                ObjectAnimator mover = ObjectAnimator.ofFloat(aniView,"translationX", -500f, 0f);
+                mover.setDuration(2000);
+                ObjectAnimator fadeIn = ObjectAnimator.ofFloat(aniView, "alpha",0f, 1f);
+                fadeIn.setDuration(2000);
+                AnimatorSet animatorSet = new AnimatorSet();
+
+                animatorSet.play(mover).with(fadeIn).after(fadeOut);
+                animatorSet.start();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+/*    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, HitActivity.class);
+        startActivity(intent);
+        return true;
     }
 }
